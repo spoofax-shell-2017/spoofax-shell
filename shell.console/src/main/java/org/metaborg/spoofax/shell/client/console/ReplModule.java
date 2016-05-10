7870313d94160d9217ac7f8638cea8d916df4804
Jente Hidskes <hjdskes@gmail.com>
2016-05-10 22:33:32 +0200

    Separate core logic from ReplModule into CoreModule

--- shell.console/src/main/java/org/metaborg/spoofax/shell/client/console/ReplModule.java
+++ shell.console/src/main/java/org/metaborg/spoofax/shell/client/console/ReplModule.java
@@ -6,12 +6,9 @@ import java.util.function.Consumer;
 
 import org.metaborg.spoofax.shell.client.IDisplay;
 import org.metaborg.spoofax.shell.client.IEditor;
-import org.metaborg.spoofax.shell.commands.ICommandInvoker;
 import org.metaborg.spoofax.shell.commands.IReplCommand;
-import org.metaborg.spoofax.shell.commands.SpoofaxCommandInvoker;
-import org.metaborg.spoofax.shell.commands.SpoofaxEvaluationCommand;
+import org.metaborg.spoofax.shell.core.CoreModule;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 import com.google.inject.TypeLiteral;
 import com.google.inject.multibindings.MapBinder;
@@ -20,14 +17,15 @@ import com.google.inject.name.Names;
 /**
  * Bindings for Repl.
  */
-public class ReplModule extends AbstractModule {
+public class ReplModule extends CoreModule {
 
-    @Override
-    protected void configure() {
+    private void configureCommands() {
         MapBinder<String, IReplCommand> commandBinder =
-            MapBinder.newMapBinder(binder(), String.class, IReplCommand.class);
+                MapBinder.newMapBinder(binder(), String.class, IReplCommand.class);
         commandBinder.addBinding("exit").to(Repl.ExitCommand.class).in(Singleton.class);
+    }
 
+    private void configureUserInterface() {
         bind(TerminalUserInterface.class).in(Singleton.class);
         bind(IEditor.class).to(TerminalUserInterface.class);
         bind(IDisplay.class).to(TerminalUserInterface.class);
@@ -37,12 +35,18 @@ public class ReplModule extends AbstractModule {
         bind(OutputStream.class).annotatedWith(Names.named("err")).toInstance(System.err);
 
         bind(new TypeLiteral<Consumer<String>>() { }).annotatedWith(Names.named("onSuccess"))
-            .to(OnEvalSuccessHook.class).in(Singleton.class);
+                .to(OnEvalSuccessHook.class).in(Singleton.class);
         bind(new TypeLiteral<Consumer<String>>() { }).annotatedWith(Names.named("onError"))
-            .to(OnEvalErrorHook.class).in(Singleton.class);
-        bind(IReplCommand.class).annotatedWith(Names.named("EvalCommand"))
-            .to(SpoofaxEvaluationCommand.class).in(Singleton.class);
-        bind(ICommandInvoker.class).to(SpoofaxCommandInvoker.class);
+                .to(OnEvalErrorHook.class).in(Singleton.class);
+    }
+
+    @Override
+    protected void configure() {
+        super.configure();
+
+        configureCommands();
+        configureUserInterface();
+
         bind(Repl.class).in(Singleton.class);
     }
 }
