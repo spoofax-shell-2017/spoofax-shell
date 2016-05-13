7abdf2b323f919f60dfc11f2d88ce12f80632123
Jente Hidskes <hjdskes@gmail.com>
2016-05-13 11:54:05 +0200

    Console: fix build

--- org.metaborg.spoofax.shell.console/src/main/java/org/metaborg/spoofax/shell/client/console/ConsoleRepl.java
+++ org.metaborg.spoofax.shell.console/src/main/java/org/metaborg/spoofax/shell/client/console/ConsoleRepl.java
@@ -10,22 +10,31 @@ import org.metaborg.spoofax.shell.core.StyledText;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-public class ConsoleRepl {
+/**
+ * This class launches a console based REPL.
+ *
+ * It uses a GNU Readline-like input buffer with multiline editing capabilities, keyboard shortcuts
+ * and persistent history. ANSI color codes are used to display colors.
+ */
+public final class ConsoleRepl {
     private static Injector injector;
 
     static {
         injector = Guice.createInjector(new ConsoleReplModule());
     }
 
+    private ConsoleRepl() {
+    }
+
     /**
      * @param args
      *            Unused.
      * @throws IOException
-     *             when an IO error occurs.
+     *             When an IO error occurs during execution.
      */
     public static void main(String[] args) throws IOException {
-        StyledText message = new StyledText(Color.BLUE, "Welcome to the ").append(Color.GREEN, "Spoofax")
-                .append(Color.BLUE, " REPL");
+        StyledText message = new StyledText(Color.BLUE, "Welcome to the ")
+            .append(Color.GREEN, "Spoofax").append(Color.BLUE, " REPL");
         injector.getInstance(IDisplay.class).displayResult(message);
         injector.getInstance(Repl.class).run();
     }
