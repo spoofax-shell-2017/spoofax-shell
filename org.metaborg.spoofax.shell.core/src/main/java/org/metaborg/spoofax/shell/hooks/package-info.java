7c68d184b9a40ec3ad6cd93c8afb8c62976f5bea
Jente Hidskes <hjdskes@gmail.com>
2016-05-27 19:55:44 +0200

    Core: provide package-info.java in all packages

---
+++ org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/hooks/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * The {@link org.metaborg.spoofax.shell.core.Repl} class uses hooks to pass results back to the
+ * client. This package defines two such hooks.
+ */
+package org.metaborg.spoofax.shell.hooks;
---
+++ org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/output/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package defines result classes, which are passed on to hooks.
+ *
+ * @see {@link org.metaborg.spoofax.shell.hooks}
+ */
+package org.metaborg.spoofax.shell.output;
