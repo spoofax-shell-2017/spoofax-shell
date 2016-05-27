eeb6a16d8c7cc89775696ee93f7e773f334779c1
Jente Hidskes <hjdskes@gmail.com>
2016-05-27 20:44:03 +0200

    Console: test the hooks

---
+++ org.metaborg.spoofax.shell.console/src/test/java/org/metaborg/spoofax/shell/client/console/impl/hooks/ConsoleMessageHookTest.java
@@ -0,0 +1,44 @@
+package org.metaborg.spoofax.shell.client.console.impl.hooks;
+
+import static org.mockito.Mockito.RETURNS_MOCKS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.metaborg.spoofax.shell.client.console.IDisplay;
+import org.metaborg.spoofax.shell.output.StyledText;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Tests {@link ConsoleMessageHook}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ConsoleMessageHookTest {
+    @Mock
+    private StyledText text;
+
+    private ConsoleMessageHook hook;
+    private IDisplay display;
+
+    /**
+     * Sets up the test environment.
+     */
+    @Before
+    public void setUp() {
+        this.display = mock(IDisplay.class, RETURNS_MOCKS);
+        this.hook = new ConsoleMessageHook(display);
+    }
+
+    /**
+     * Tests if {@link IDisplay#displayResult(StyledText)} is called correctly.
+     */
+    @Test
+    public void testAccept() {
+        hook.accept(text);
+        verify(display, times(1)).displayResult(text);
+    }
+}
---
+++ org.metaborg.spoofax.shell.console/src/test/java/org/metaborg/spoofax/shell/client/console/impl/hooks/ConsoleResultHookTest.java
@@ -0,0 +1,50 @@
+package org.metaborg.spoofax.shell.client.console.impl.hooks;
+
+import static org.mockito.Mockito.RETURNS_MOCKS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.metaborg.spoofax.shell.client.console.IDisplay;
+import org.metaborg.spoofax.shell.output.ISpoofaxResult;
+import org.metaborg.spoofax.shell.output.StyledText;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Tests {@link ConsoleResultHook}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ConsoleResultHookTest {
+    @Mock
+    private StyledText text;
+    @Mock
+    private ISpoofaxResult<?> result;
+
+    private ConsoleResultHook hook;
+    private IDisplay display;
+
+    /**
+     * Sets up the test environment.
+     */
+    @Before
+    public void setUp() {
+        this.display = mock(IDisplay.class, RETURNS_MOCKS);
+        this.hook = new ConsoleResultHook(display);
+
+        when(result.styled()).thenReturn(text);
+    }
+
+    /**
+     * Tests if {@link IDisplay#displayResult(StyledText)} is called correctly.
+     */
+    @Test
+    public void testAccept() {
+        hook.accept(result);
+        verify(display, times(1)).displayResult(text);
+    }
+}
