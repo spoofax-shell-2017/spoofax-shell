abb35aeebd06c4c6e09f2fe0e309e83ff78b92dd
Balletie <skip_meesie@hotmail.com>
2016-06-05 15:03:41 +0200

    Use lang3.reflect instead of beanutils for MethodUtils class

--- org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/core/DynSemEvaluationStrategy.java
+++ org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/core/DynSemEvaluationStrategy.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.beanutils.MethodUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.ClassUtils;
 import org.metaborg.core.MetaborgException;
