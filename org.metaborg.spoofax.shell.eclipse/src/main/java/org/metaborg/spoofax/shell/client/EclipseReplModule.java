aa8152c7cb315f77ebac97b9c0990cf9892c99eb
Jente Hidskes <hjdskes@gmail.com>
2016-06-23 00:22:48 +0200

    Eclipse: add an ExitCommand

--- org.metaborg.spoofax.shell.eclipse/src/main/java/org/metaborg/spoofax/shell/client/EclipseReplModule.java
+++ org.metaborg.spoofax.shell.eclipse/src/main/java/org/metaborg/spoofax/shell/client/EclipseReplModule.java
@@ -2,15 +2,24 @@ package org.metaborg.spoofax.shell.client;
 
 import org.metaborg.spoofax.shell.ReplModule;
 import org.metaborg.spoofax.shell.client.eclipse.ColorManager;
+import org.metaborg.spoofax.shell.client.eclipse.commands.ExitCommand;
 import org.metaborg.spoofax.shell.client.eclipse.impl.IWidgetFactory;
+import org.metaborg.spoofax.shell.commands.IReplCommand;
 
 import com.google.inject.Singleton;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.multibindings.MapBinder;
 
 /**
  * Bindings for the Eclipse REPL.
  */
 public class EclipseReplModule extends ReplModule {
+
+    @Override
+    protected void bindCommands(MapBinder<String, IReplCommand> commandBinder) {
+        super.bindCommands(commandBinder);
+        commandBinder.addBinding("exit").to(ExitCommand.class);
+    }
 
     @Override
     protected void configure() {
