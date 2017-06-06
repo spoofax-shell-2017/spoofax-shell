cdd4cace36d6e45ef7961dd5f8b6e1736d1846ab
Justin van der Krieken <justin@vdkrieken.com>
2017-06-06 10:52:30 +0200

    Remove unused imports

--- org.metaborg.spoofax.shell.eclipse/src/main/java/org/metaborg/spoofax/shell/client/EclipseReplModule.java
+++ org.metaborg.spoofax.shell.eclipse/src/main/java/org/metaborg/spoofax/shell/client/EclipseReplModule.java
@@ -1,20 +1,13 @@
 package org.metaborg.spoofax.shell.client;
 
-import org.apache.commons.vfs2.FileObject;
-import org.metaborg.core.MetaborgException;
-import org.metaborg.core.project.IProject;
 import org.metaborg.core.project.ISimpleProjectService;
 import org.metaborg.core.project.SimpleProjectService;
-import org.metaborg.core.resource.IResourceService;
-import org.metaborg.core.resource.ResourceService;
 import org.metaborg.spoofax.shell.ReplModule;
 import org.metaborg.spoofax.shell.client.eclipse.ColorManager;
 import org.metaborg.spoofax.shell.client.eclipse.commands.ExitCommand;
 import org.metaborg.spoofax.shell.client.eclipse.impl.IWidgetFactory;
 import org.metaborg.spoofax.shell.commands.IReplCommand;
 
-import com.google.common.io.Files;
-import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.multibindings.MapBinder;
