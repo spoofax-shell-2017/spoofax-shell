393239d1111b1a34a1db777d120907293168acfd
Pathemeous <pathemeous@gmail.com>
2017-06-13 14:21:42 +0200

    Add Editor Services interface
    Provides Editor services that directly invoke the FailableFunctions by
    using the FunctionComposer. The Editor Services can be injected using
    Guice, to the frontend and used in a similar way to the CommandInvoker.
    The presence of a separate class for this, is that the exact result type
    can be determined, while with IReplCommands this was not the case.
    
    The Editor uses a strategy pattern to define its behaviour before and
    after loading a language. This is necessary to provide a clean interface
    to the client, who must receive its EditorServices object from the
    start, even though a language is not loaded initially, so that it
    doesn't need to do null checks all the time.
    The strategy of the EditorServices will decide whether it can provide a
    meaningful result or simply return an ExceptionResult.

---
+++ org.metaborg.spoofax.shell.core/src/test/java/org/metaborg/spoofax/shell/services/SpoofaxEditorServicesTest.java
@@ -0,0 +1,116 @@
+package org.metaborg.spoofax.shell.services;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.metaborg.spoofax.shell.functions.FunctionComposer;
+import org.metaborg.spoofax.shell.output.FailOrSuccessResult;
+import org.metaborg.spoofax.shell.output.IResult;
+import org.metaborg.spoofax.shell.output.StyleResult;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+/**
+ * Test the default implementation of {@link IEditorServices}.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SpoofaxEditorServicesTest {
+
+	private SpoofaxEditorServices services;
+
+	@Mock
+	private FunctionComposer composer;
+	@Mock
+	private IServicesStrategyFactory factory;
+	@Mock
+	private UnloadedServices unloaded;
+	@Mock
+	private LoadedServices loaded;
+	@Mock
+	private FailOrSuccessResult<StyleResult, IResult> highlightResult;
+
+	/**
+	 * Set up instance of the class under test.
+	 */
+	@Before
+	public void setUp() {
+		when(factory.createUnloadedStrategy()).thenReturn(unloaded);
+		when(factory.createLoadedStrategy(any())).thenReturn(loaded);
+		services = new SpoofaxEditorServices(factory);
+	}
+
+	/**
+	 * Test that Services are constructed with 'unloaded' behaviour.
+	 */
+	@Test
+	public void testSpoofaxEditorServices() {
+		verify(factory).createUnloadedStrategy();
+	}
+
+	/**
+	 * Tests that the Services' strategy is correctly replaced when a language
+	 * is loaded.
+	 */
+	@Test
+	public void testLoad() {
+		services.load(composer);
+		verify(factory).createLoadedStrategy(composer);
+		services.isLoaded();
+		verify(loaded).isLoaded();
+	}
+
+	/**
+	 * Tests the strategy delegation of
+	 * {@link IEditorServices#highlight(String)} to the unloaded strategy.
+	 */
+	@Test
+	public void testHighlightUnloaded() {
+		final String testSource = "test";
+		services.highlight(testSource);
+		verifyZeroInteractions(loaded);
+		verify(unloaded).highlight(testSource);
+	}
+
+	/**
+	 * Tests the strategy delegation of
+	 * {@link IEditorServices#highlight(String)} to the loaded strategy.
+	 */
+	@Test
+	public void testHighlightLoaded() {
+		final String testSource = "test";
+		services.load(composer);
+		services.highlight(testSource);
+		verifyZeroInteractions(unloaded);
+		verify(loaded).highlight(testSource);
+	}
+
+	/**
+	 * Tests the strategy delegation of {@link IEditorServices#isLoaded()} as well as the unloaded
+	 * return value (false).
+	 */
+	@Test
+	public void testIsLoadedFalse() {
+		services.isLoaded();
+		verify(unloaded).isLoaded();
+	}
+
+	/**
+	 * Tests the strategy delegation of {@link IEditorServices#isLoaded()} as well as the loaded
+	 * return value (true).
+	 */
+	@Test
+	public void testIsLoadedTrue() {
+		FunctionComposer composer = mock(FunctionComposer.class);
+		services.load(composer);
+		services.isLoaded();
+		verifyZeroInteractions(unloaded);
+		verify(loaded).isLoaded();
+	}
+
+}
